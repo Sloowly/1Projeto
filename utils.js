e605d7a7faaf054e5fbb3175e7a0e9365c23c4dc
Sloowly <edsonshb@gmail.com>
2017-05-24 21:01:12 -0300

    add new functions and css file to project

--- utils.js
+++ utils.js
@@ -1,5 +1,20 @@
-function multiplyTwoNumbers(firstNumber,secondNumber) {
+function multiplyTwoNumbers(x,y) {
     var result;
-    result = firstNumber * secondNumber;
+    result = x * y;
+    return result;
+}
+
+var message = ('Hello World')
+function showName (start) {console.log(message); }
+
+function divideTwoNumbers(x,y) {
+    var result;
+    result = x / y;
+    return result;
+}
+
+function sumTwoNumers(x,y) {
+    var resut;
+    result = x + y;
     return result;
 }
